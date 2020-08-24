f7bad00a38e7c620d569c52b55d63822ed9e3e96
Sohailmcs <Sohail.mcs@gmail.com>
2020-08-24 22:09:50 +0500

    error

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const { Menu } = require("electron");
 const { app, BrowserWindow } = electron;
 
 let mainWindow;
+let addWindow;
 
 // Listen for app to be ready
 app.on("ready", () => {
@@ -23,6 +24,26 @@ app.on("ready", () => {
 
   Menu.setApplicationMenu(mainMenu);
 });
+
+function createAddWindow() {
+  // create new window
+  addWindow = new BrowserWindow({
+    width: 200,
+    height: 300,
+    title: "Add new student",
+  });
+  // load html into window
+  addWindow.loadURL(
+    url.format({
+      pathname: path.join(__dirname, "addWindow.html"),
+      protocol: "file:",
+      slashes: true,
+    })
+  );
+}
+
+// getElementbyId('addstudent').
+//document.getElementById("addstu").innerHTML = createAddWindow();
 
 const mainaMenuTemplate = [
   {
